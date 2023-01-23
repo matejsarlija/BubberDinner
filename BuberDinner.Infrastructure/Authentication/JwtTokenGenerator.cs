54910dbecbd1e64620a03d081e8036789e76070f
matejsarlija <matejsarlija1@gmail.com>
2023-01-23 17:40:02 +0100

    signing credentials

--- BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -1,4 +1,8 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BuberDinner.Infrastructure.Authentication;
 
@@ -6,6 +10,26 @@ public class JwtTokenGenerator : IJwtTokenGenerator
 {
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
-        throw new NotImplementedException();
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("super-secret-key")
+            ), SecurityAlgorithms.HmacSha256
+        );
+
+        var claims = new []
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: "BuberDinner",
+            expires: DateTime.Now.AddDays(1),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
     }
 }
